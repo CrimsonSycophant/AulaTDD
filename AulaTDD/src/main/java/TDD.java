65253d822bbaa02a79535440a07ef7e4d7e1182d
CrimsonSycophant <54426860+CrimsonSycophant@users.noreply.github.com>
2019-11-04 13:00:57 -0300

    Passo 5

--- AulaTDD/src/main/java/TDD.java
+++ AulaTDD/src/main/java/TDD.java
@@ -1,40 +1,24 @@
-abstract class Money{
+class Money{
     protected int amount;
     private String currency;
     public boolean equals(Object object){
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
     static Money dollar(int amount){
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
     static Money franc(int amount){
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
-    abstract Money times(int multiplier);
-    String currency(){
-        return currency;
+    Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
     }
     Money(int amount, String currency){
         this.amount = amount;
         this.currency = currency;
     }
-}
-
-class Dollar extends Money{
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }			
-    Money times(int multiplier) {
-        return Money.dollar(amount * multiplier);
-    }
-}
-
-class Franc extends Money{
-    Franc(int amount, String currency) {
-       super(amount, currency);
-    }
-    Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+    String currency(){
+        return currency;
     }
 }
